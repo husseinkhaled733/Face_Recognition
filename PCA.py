425bcbd3cecbaba3544407ee22ab94b9bed69a5f
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2024-03-03 19:28:54 +0200

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -4,9 +4,9 @@
 * D is the train data matrix with size n x d, n = 200 and d = 10304
 """
 import numpy as np
-from numpy.linalg import eig
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+import cupy as cp
 
 labels_num = 40  # The number of persons
 features_num = 10304  # The number of features in every picture
@@ -15,7 +15,7 @@ dataset_dir = "archive\\"  # The dataset directory path
 
 def read_image(image_path):
     """
-    :param image_path: path to image
+    :param: image_path: path to image
     :return: A list of length 10304 contains the features of this image
     """
     image_vec = []
@@ -62,34 +62,37 @@ def split_data(ds, lb):
     return np.array(trd), np.array(trl), np.array(tsd), np.array(tsl)
 
 
-def PCA(ds, alpha):
+def get_eigen_variables(ds):
     """
     :param ds: Dataset (n x d) (200 x 10304)
-    :param alpha:
-    :return:
+    :return eigenvalues, eigenvectors: Those are for the covariance matrix of the dataset
     """
-    n, d = len(ds), len(ds[0])  # n: number of samples, d: dimensionality
 
     # 1) Calculate the mean vector
     means = np.mean(ds, axis=0, dtype=np.float64)
 
     # 2) Center the data
-    z = [[0] * d for _ in range(n)]
-    for i in range(n):  # images
-        for j in range(d):  # features
-            z[i][j] = ds[i][j] - means[j]
+    z = ds - means
     z = np.array(z).transpose()
 
     # 3) Calculate the covariance matrix
-    cov = np.cov(z, bias=True)
+    cov = cp.cov(z, bias=True)
 
     # 4) Get the eigenvalues and eigenvectors of the covariance matrix in decreasing order of eigen values
-    eigenvalues, eigenvectors = eig(cov)
+    eigenvalues, eigenvectors = cp.linalg.eigh(cov)
     idx = eigenvalues.argsort()[::-1]
-    eigenvalues = np.array(eigenvalues[idx])
-    eigenvectors = np.array(eigenvectors[:, idx]).transpose()
+    eigenvalues = cp.ndarray.get(eigenvalues[idx])
+    eigenvectors = cp.ndarray.get(eigenvectors[:, idx]).transpose()
+    return eigenvalues, eigenvectors
 
-    # 5) Get the projection matrix
+
+def get_projection_matrix(eigenvalues, eigenvectors, alpha):
+    """
+    :param eigenvalues:
+    :param eigenvectors:
+    :param alpha: Threshold for significance
+    :return projection_matrix: The matrix used for reducing data dimensions
+    """
     p = []  # p: projection matrix
     sm, total = 0, sum(eigenvalues)  # sm: sum of eigenvalues, total: total sum of all eigenvalues
     for eigenvalue, eigenvector in zip(eigenvalues, eigenvectors):
@@ -104,19 +107,33 @@ def PCA(ds, alpha):
     return np.array(p).transpose()
 
 
+def PCA(ds, alpha):
+    """
+    :param ds: Dataset (n x d) (200 x 10304)
+    :param alpha:
+    :return projection_matrix: The matrix used for reducing data dimensions
+    """
+    eigenvalues, eigenvectors = get_eigen_variables(ds)
+    return get_projection_matrix(eigenvalues, eigenvectors, alpha)
+
+
 dataset, labels = construct_dataset()
 
 train_data, train_labels, test_data, test_labels = split_data(dataset, labels)
+alphas = [0.8, 0.85, 0.9, 0.95]
+e_val, e_vec = get_eigen_variables(train_data)
+# Comment out next line when testing multiple alphas, uncomment the next line when testing one alpha
+# projection_matrix = PCA(train_data, 0.8)
+for a in alphas:
+    # Comment out next line and remove the loop when testing one alpha, un-comment and add the loop for multiple alphas
+    projection_matrix = get_projection_matrix(e_val, e_vec, a)
+    projected_train_data = np.real(np.dot(train_data, projection_matrix))
+    projected_test_data = np.real(np.dot(test_data, projection_matrix))
 
-projection_matrix = PCA(train_data, 0.8)
-
-projected_train_data = np.real(train_data.dot(projection_matrix))
-projected_test_data = np.real(test_data.dot(projection_matrix))
-
-K = [1, 3, 5, 7]
-for i in K:
-    knn = KNeighborsClassifier(n_neighbors=i)
-    knn.fit(projected_train_data, train_labels)
-    predicted_labels = knn.predict(projected_test_data)
-    acc = accuracy_score(test_labels, predicted_labels)
-    print("Accuracy = ", acc, "  at k = ", i)
+    K = [1, 3, 5, 7]
+    for k in K:
+        knn = KNeighborsClassifier(n_neighbors=k)
+        knn.fit(projected_train_data, train_labels)
+        predicted_labels = knn.predict(projected_test_data)
+        acc = accuracy_score(test_labels, predicted_labels)
+        print("Accuracy = ", acc, "  at k = ", k, " and alpha = ", a)
